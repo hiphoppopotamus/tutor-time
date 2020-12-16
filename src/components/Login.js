a5facbc985c9e0c27af4dd1ab4978f2eddb2aafe
Euan Widjaja <euanwidjaja@yahoo.co.nz>
2020-12-17 09:53:29 +1300

    add handlechange for logging in

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,14 +2,26 @@ import React, { Component } from 'react';
 import '../css/Login.css';
 
 class Login extends Component {
+    state = {
+        email: null,
+        password: null
+    }
+
+    handleChange(event) {
+        this.setState({
+            email: event.target.email,
+            password: event.target.password,
+        })
+    }
+
     render() {
         return (
             <div className="login-container">
                 <form className="login-form" onSubmit={this.handleSubmit}>
                     <label htmlFor="email">Email:</label>
-                    <input placeholder="Email" type="text" id="email" />
+                    <input placeholder="Email" type="text" id="email" value={this.state.email} />
                     <label htmlFor="password">Password:</label>
-                    <input placeholder="Password" type="text" id="password" />
+                    <input placeholder="Password" type="text" id="password" value={this.state.password} />
                     <button type="submit">Submit</button>
                 </form>
             </div>
